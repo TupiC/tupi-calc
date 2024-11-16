44b1d47b9054d8be3dd2d14555f3c075e194e42b
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2024-11-16 12:41:22 +0100

    feat: possibility to disable extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,27 @@ import * as vscode from 'vscode';
 import { calculateExpression } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
+
+	function isExtensionDisabled(): boolean {
+		const config = vscode.workspace.getConfiguration('tupicalc');
+		return config.get('isDisabled') === true;
+	}
+
+	let toggleCommand = vscode.commands.registerCommand('tupicalc.toggleExtension', () => {
+		const config = vscode.workspace.getConfiguration('tupicalc');
+		const currentState = config.get('isDisabled');
+		config.update('isDisabled', !currentState, true).then(() => {
+			vscode.window.showInformationMessage(`TupiCalc extension is now ${!currentState ? 'disabled' : 'enabled'}`);
+		});
+	});
+
 	let decorationType: vscode.TextEditorDecorationType;
 
 	const updateDecorations = () => {
+		if (isExtensionDisabled()) {
+			return;
+		}
+
 		const editor = vscode.window.activeTextEditor;
 
 		if (!editor) {
@@ -25,6 +43,7 @@ export function activate(context: vscode.ExtensionContext) {
 	updateDecorations();
 
 	context.subscriptions.push(
+		toggleCommand,
 		vscode.window.onDidChangeActiveTextEditor(updateDecorations),
 		vscode.workspace.onDidChangeTextDocument(event => {
 			if (event.document === vscode.window.activeTextEditor?.document) {
@@ -32,6 +51,12 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		})
 	);
+
+	vscode.workspace.onDidChangeConfiguration(e => {
+		if (e.affectsConfiguration('tupicalc.isDisabled')) {
+			updateDecorations();
+		}
+	});
 }
 
 export function deactivate() {
